5c56ee0b28191532f9c4f825debcd3eb9af2a14d
olympia <owojcik7@gmail.com>
2019-03-22 11:59:52 -0400

    Add ability to get all projects in projectsRouter

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -5,7 +5,11 @@ const db = require("./projectModel.js");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Projects router workin");
+  db.get()
+    .then(projects => res.status(200).json(projects))
+    .catch(err =>
+      res.status(400).json({ errorMessage: "Error - cannot get projects" })
+    );
 });
 
 module.exports = router;
