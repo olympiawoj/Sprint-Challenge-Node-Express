21ecfc2b5fd35fe6f02f0b7a7c193037b98d256d
olympia <owojcik7@gmail.com>
2019-03-22 11:32:55 -0400

    Get server running

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+
+const port = 5000;
+
+const server = require("./server.js");
+
+server.listen(port, () => console.log(`<-----API running on:${port}`));
